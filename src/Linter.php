2216a8228f72c7e5376d2d2cca5c1ab20a9fd386
Alex Goncalves Rodrigues <144851148+GONCALVES-RODRIGUES-Alex-2326052@users.noreply.github.com>
2025-02-24 10:11:01 +0100

    Finished Linter, testes comming

---
+++ src/Linter.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace GONCALVESRODRIGUES\SecurityLinter;
+class Linter{
+    private static $rules = [
+        'Protection XSS' => '/\$_(GET|POST|REQUEST|COOKIE)\[.*?\](?!.*htmlspecialchars|strip_tags)/',
+        'SQL Injection'  => '/mysqli_query\(.*?\$.*?\)/'
+    ];
+
+    /**
+     * Scanne le dossier et retourne une liste des problemes de securité a regler.
+     *
+     * @param string $path
+     * @return array
+     */
+    public static function scanProjet($path)
+    {
+        $issues = [];
+        $files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path));
+
+        foreach($files as $file){
+            if (pathinfo($file, PATHINFO_EXTENSION) == 'php') {
+                $issuescan = self::scanFiles($file);
+                if(!empty($issuescan)){
+                    $issues[$file] = $issuescan;
+                }
+            }
+        }
+        return $issues;
+    }
+
+    /**
+     * Analyse un fichier PHP a la recherche d'erreurs de secu.
+     *
+     * @param string $filePath
+     * @return array
+     */
+    public static function scanFiles($filePath)
+    {
+        $issues = [];
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach($lines as $i => $line){
+            foreach(self::$rules as $ruleName => $pattern){
+                if(preg_match($pattern, $line)){
+                    $issues[] = "$ruleName -> $filePath (Ligne " . ($i + 1) . "): $line";
+                }
+            }
+        }
+        return $issues;
+    }
+}
